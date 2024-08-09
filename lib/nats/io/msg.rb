cad8b93999d07839daf7644b07d7edc658cc1bc6
thomas morgan <tm@iprog.com>
2024-08-09 13:05:21 -0600

    fix ruby warnings
    tested on ruby 3.3

--- lib/nats/io/msg.rb
+++ lib/nats/io/msg.rb
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-require_relative 'jetstream'
+require_relative 'jetstream/msg'
 
 module NATS
   class Msg
