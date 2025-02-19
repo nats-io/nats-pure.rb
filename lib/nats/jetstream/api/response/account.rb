f45eb4a64d2e8e98baa6bfe09005a961bb0ad168
Julia <j.e.egorova@gmail.com>
2025-02-19 13:04:46 +0300

    Update AccountInfoResponse

--- lib/nats/jetstream/api/response/account.rb
+++ lib/nats/jetstream/api/response/account.rb
@@ -4,6 +4,46 @@ module NATS
   class JetStream
     class API
       class AccountInfoResponse < Response
+        # Memory Storage being used for Stream Message storage
+        integer :memory
+
+        # File Storage being used for Stream Message storage
+        integer :storage
+
+        # Number of active Streams
+        integer :streams
+
+        # Number of active Consumers
+        integer :consumers
+
+        # The JetStream domain this account is in
+        string :domain
+
+        object :limits, of: Consumer::Limits
+
+        object :tiers do
+          # Memory Storage being used for Stream Message storage
+          integer :memory
+
+          # File Storage being used for Stream Message storage
+          integer :storage
+
+          # Number of active Streams
+          integer :streams
+
+          # Number of active Consumers
+          integer :consumers
+
+          object :limits, of: Consumer::Limits
+        end
+
+        object :api do
+          # Total number of API requests received for this account
+          integer :total
+
+          # API requests that resulted in an error response
+          integer :errors
+        end
       end
     end
   end
---
+++ lib/nats/jetstream/consumer/schemas/limits.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module NATS
+  class JetStream
+    class Consumer
+      class Limits < NATS::Utils::Config
+        # The maximum amount of Memory storage Stream Messages may consume
+        integer :max_memory
+
+        # The maximum amount of File storage Stream Messages may consume
+        integer :max_storage
+
+        # The maximum number of Streams an account can create
+        integer :max_streams
+
+        # The maximum number of Consumer an account can create
+        integer :max_consumers
+
+        # Indicates if Streams created in this account requires the max_bytes property set
+        bool :max_bytes_required
+
+        # The maximum number of outstanding ACKs any consumer may configur
+        integer :max_ack_pending
+
+        # The maximum size any single memory stream may be
+        integer  :memory_max_stream_bytes, default: -1
+
+        # The maximum size any single storage based stream may be
+        integer :storage_max_stream_bytes, default: -1
+      end
+    end
+  end
+end
